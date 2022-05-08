b47286612fb0d7a7a95ad045da9cee3feade9215
SichongP <scpeng@ucdavis.edu>
2022-05-08 11:52:29 -0700

    remove hard coded file path

--- workflow/scripts/pyMergeBioSamples.py
+++ workflow/scripts/pyMergeBioSamples.py
@@ -2,13 +2,13 @@ import numpy as np
 import pandas as pd
 import glob, os
 
-peak_files = snakemake.input['peaks']
+peak_files = snakemake.input['peak_files']
 outfile = snakemake.output[0]
 
 def merge_bio_samples(files, outfile = '.'):
     peaks = pd.DataFrame()
     for file in files:
-        peaks = pd.concat([peaks, pd.read_csv("../../results/peaks/macs3/standard/AH1_Adipose/AH1_Adipose.unique_501bp_peaks.txt", index_col = False)])
+        peaks = pd.concat([peaks, pd.read_csv(file, index_col = False)])
     peaks = peaks.sort_values('normalized_score', ascending = False)
     while i < peaks.shape[0]:
         interval = pd.arrays.IntervalArray.from_arrays(left = peaks.iloc[i+1:]['start'], right = peaks.iloc[i+1:]['end'], closed = 'both')
