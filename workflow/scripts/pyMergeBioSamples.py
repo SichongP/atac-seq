eeab49e05e0b3a732c7341e6a544152d7bb53e87
SichongP <scpeng@ucdavis.edu>
2022-05-08 11:57:37 -0700

    fix: init i

--- workflow/scripts/pyMergeBioSamples.py
+++ workflow/scripts/pyMergeBioSamples.py
@@ -10,6 +10,7 @@ def merge_bio_samples(files, outfile = '.'):
     for file in files:
         peaks = pd.concat([peaks, pd.read_csv(file, index_col = False)])
     peaks = peaks.sort_values('normalized_score', ascending = False)
+    i = 0
     while i < peaks.shape[0]:
         interval = pd.arrays.IntervalArray.from_arrays(left = peaks.iloc[i+1:]['start'], right = peaks.iloc[i+1:]['end'], closed = 'both')
         peaks = peaks[~np.insert(interval.overlaps(pd.Interval(left = peaks.iloc[i]['start'], right = peaks.iloc[i]['end'], closed = 'both')), 0, [False]*(i+1))]
